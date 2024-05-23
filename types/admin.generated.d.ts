1bf6057f86ac0f8126b421a98266a11dacf362f0
Adam Ryan Merrifield <adam.merrifield@gmail.com>
2024-05-23 00:30:15 -0400

    Update admin.generated.d.ts

--- types/admin.generated.d.ts
+++ types/admin.generated.d.ts
@@ -3,21 +3,41 @@
 /* eslint-disable */
 import * as AdminTypes from './admin.types.d.ts';
 
-export type ProductQueryQueryVariables = AdminTypes.Exact<{
-  id: AdminTypes.Scalars['ID']['input'];
+export type ProductUpdateMutationVariables = AdminTypes.Exact<{
+  input: AdminTypes.ProductInput;
 }>;
 
 
-export type ProductQueryQuery = { product?: AdminTypes.Maybe<(
-    Pick<AdminTypes.Product, 'id' | 'title' | 'handle'>
-    & { metafields: { edges: Array<{ node: Pick<AdminTypes.Metafield, 'key' | 'value'> }> } }
+export type ProductUpdateMutation = { productUpdate?: AdminTypes.Maybe<{ product?: AdminTypes.Maybe<{ metafields: { edges: Array<{ node: Pick<AdminTypes.Metafield, 'id' | 'namespace' | 'key' | 'value'> }> } }> }> };
+
+export type MetafieldDeleteMutationVariables = AdminTypes.Exact<{
+  input: AdminTypes.MetafieldDeleteInput;
+}>;
+
+
+export type MetafieldDeleteMutation = { metafieldDelete?: AdminTypes.Maybe<(
+    Pick<AdminTypes.MetafieldDeletePayload, 'deletedId'>
+    & { userErrors: Array<Pick<AdminTypes.UserError, 'field' | 'message'>> }
   )> };
 
+export type ProductsQueryQueryVariables = AdminTypes.Exact<{
+  numProducts: AdminTypes.Scalars['Int']['input'];
+  cursor?: AdminTypes.InputMaybe<AdminTypes.Scalars['String']['input']>;
+}>;
+
+
+export type ProductsQueryQuery = { products: { nodes: Array<(
+      Pick<AdminTypes.Product, 'id' | 'title' | 'handle'>
+      & { metafield?: AdminTypes.Maybe<Pick<AdminTypes.Metafield, 'value'>> }
+    )>, pageInfo: Pick<AdminTypes.PageInfo, 'hasNextPage' | 'endCursor'> } };
+
 interface GeneratedQueryTypes {
-  "#graphql\n  query ProductQuery($id: ID!) {\n    product(id: $id) {\n      id\n      title\n      handle\n\n      metafields (first:100) {\n        edges {\n          node {\n            key\n            value\n          }\n        }\n      }\n    }\n  }\n": {return: ProductQueryQuery, variables: ProductQueryQueryVariables},
+  "#graphql\n  query ProductsQuery($numProducts: Int!, $cursor: String){\n    products(first: $numProducts, after: $cursor) {\n      nodes {\n        id\n        title\n        handle\n\n        metafield(namespace: \"customizer\", key: \"Paper Stock\") {\n          value\n        }\n      }\n      pageInfo {\n        hasNextPage\n        endCursor\n      }\n    }\n  }\n": {return: ProductsQueryQuery, variables: ProductsQueryQueryVariables},
 }
 
 interface GeneratedMutationTypes {
+  "#graphql\n  mutation productUpdate($input: ProductInput!) {\n    productUpdate(input: $input) {\n      product {\n        metafields(first: 100) {\n          edges {\n            node {\n              id\n              namespace\n              key\n              value\n            }\n          }\n        }\n      }\n    }\n  }\n": {return: ProductUpdateMutation, variables: ProductUpdateMutationVariables},
+  "#graphql\n  mutation metafieldDelete($input: MetafieldDeleteInput!) {\n    metafieldDelete(input: $input) {\n      deletedId\n      userErrors {\n        field\n        message\n      }\n    }\n  }\n": {return: MetafieldDeleteMutation, variables: MetafieldDeleteMutationVariables},
 }
 declare module '@shopify/admin-api-client' {
   type InputMaybe<T> = AdminTypes.InputMaybe<T>;
