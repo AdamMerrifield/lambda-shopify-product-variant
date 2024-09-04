dd0cc4b1bfb6ff9618898b1f0624d6bddb0108e9
Adam Ryan Merrifield <adam.merrifield@gmail.com>
2024-09-03 23:02:56 -0400

    use put

--- src/s3-utils.ts
+++ src/s3-utils.ts
@@ -1,9 +1,9 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 
 export async function getPresignedUploadUrlForLogo(key: string) {
   const client = new S3Client({ region: 'us-east-2' })
-  const command = new GetObjectCommand({ Bucket: 'shopify-customer-logos', Key: key })
+  const command = new PutObjectCommand({ Bucket: 'shopify-customer-logos', Key: key })
 
   return await getSignedUrl(client, command, { expiresIn: 3600 })
 }
